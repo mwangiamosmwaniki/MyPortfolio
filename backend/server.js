a6de8d879579f1a69ba3ab5870950e17ee13ff2c
Amos <mwangiamos703@gmail.com>
2026-03-07 00:06:38 +0300

    port forwarding issue fix

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,9 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS || !process.env.TO_EMAIL)
 
 // Configure transporter once
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
